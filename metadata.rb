86318a5dfcd9c600b9a817282faa1c06ef02eb52
Antonis Kouzoupis <kouzoupis.ant@gmail.com>
2020-03-31 09:33:29 +0200

    [pin_java] Pin deps

--- metadata.rb
+++ metadata.rb
@@ -13,10 +13,12 @@ supports 'ubuntu'
 supports 'debian'
 supports 'windows'
 
-depends 'java'
-depends 'authbind'
-
 depends 'archive'
 depends 'cutlery'
 depends 'runit'
 
+# Compat resource is required for 12.5+ as resource API changed between 12.4 and 12.5
+depends 'compat_resource', '~> 12.19.0'
+
+depends 'java', '~> 7.0.0'
+depends 'authbind', '~> 0.1.10'
