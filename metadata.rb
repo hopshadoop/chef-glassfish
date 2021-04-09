5f32907217c5d014517417e7638bed34c054d24b
Theofilos Kakantousis <t.kakantousis@gmail.com>
2021-04-09 15:56:33 +0200

    Release version: 2.2

--- metadata.rb
+++ metadata.rb
@@ -1,13 +1,11 @@
 name 'glassfish'
 maintainer 'Peter Donald'
 maintainer_email 'peter@realityforge.org'
-license 'Apache-2.0'
-description 'Installs/Configures GlassFish Application Server'
-version '1.2.7'
+license          'Apache 2.0'
+description      'Installs/Configures GlassFish Application Server'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "2.3.0"
 chef_version '>= 13.0'
-
-issues_url 'https://github.com/realityforge/chef-glassfish'
-source_url 'https://github.com/realityforge/chef-glassfish'
 
 supports 'ubuntu'
 supports 'debian'
